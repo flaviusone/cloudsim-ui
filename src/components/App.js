7a3a5c570a64ac4a60a25890896e11de73bd0773
Flavius Tirnacop <flaviusone@gmail.com>
2016-12-02 00:22:18 +0100

    Realign title

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import DataCenter from './DataCenter';
+
+import './App.css';
 
 class App extends Component {
   render() {
