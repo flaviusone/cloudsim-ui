425eba0f33906a227468c1b8a33d273721b97105
Flavius Tirnacop <flaviusone@gmail.com>
2016-12-03 10:58:05 +0100

    Render broker and datacenter inside main app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import DataCenter from './DataCenter';
+import Broker from './Broker';
 
 import './App.css';
 
 class App extends Component {
   render() {
+    const {dataCenter} = this.props.data
     return (
         <div className="app">
-          <AppBar title="cloudsim-ui" showMenuIconButton={false}/>
+          <AppBar
+            title="cloudsim-ui"
+            showMenuIconButton={false}/>
+          <div className="content-container">
+            <DataCenter data={dataCenter}/>
+            <Broker data={dataCenter}/>
+          </div>
         </div>
     );
   }
