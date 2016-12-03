5d5e414738ae8df4e12b16c74a59b0334cc015d5
Flavius Tirnacop <flaviusone@gmail.com>
2016-12-03 11:24:15 +0100

    Css tweaks

--- src/components/DataCenter.js
+++ src/components/DataCenter.js
@@ -4,7 +4,6 @@ import './DataCenter.css';
 
 class DataCenter extends Component {
   render() {
-    const {name, id, hosts} = this.props.data;
     return (
         <div className="data-center">
           Datacenterul
